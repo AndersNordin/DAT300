259dd98bd91fc2d111c17b6707145e326edea774
AndersNordin <anders.t.nordin@gmail.com>
2014-10-17 15:20:20 +0200

    hmm

---
+++ HomeAutomation/Settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Oct 16, 2014
+
+@author: anders
+'''
+
+CIRCLEPLUS_MAC = "000D6F0001A44DE1"
+CIRCLE1_MAC = "000D6F0001A44958"
+USB_PORT = "/dev/ttyUSB0"
